f6265dd2c001909862f15328729e390d335d1ac1
Michael Fairley <michaelfairley@gmail.com>
2013-08-18 17:05:07 -0500

    Stationary ball

---
+++ ball.rb
@@ -0,0 +1,25 @@
+class Ball
+  SIZE = 16
+
+  attr_reader :x, :y
+  def initialize
+    @x = Pong::WIDTH/2
+    @y = Pong::HEIGHT/2
+  end
+
+  def x1; @x - SIZE/2; end
+  def x2; @x + SIZE/2; end
+  def y1; @y - SIZE/2; end
+  def y2; @y + SIZE/2; end
+
+  def draw(window)
+    color = Gosu::Color::RED
+
+    window.draw_quad(
+      x1, y1, color,
+      x1, y2, color,
+      x2, y2, color,
+      x2, y1, color,
+    )
+  end
+end
--- pong.rb
+++ pong.rb
@@ -1,12 +1,22 @@
 require "bundler/setup"
 require "hasu"
 
+Hasu.load "ball.rb"
+
 class Pong < Hasu::Window
   WIDTH = 768
   HEIGHT = 576
 
   def initialize
     super(WIDTH, HEIGHT, false)
+  end
+
+  def reset
+    @ball = Ball.new
+  end
+
+  def draw
+    @ball.draw(self)
   end
 end
 
