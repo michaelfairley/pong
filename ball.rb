931d6bf8a1c761d8516b60693e3f302912d355fe
Michael Fairley <michaelfairley@gmail.com>
2013-08-18 20:21:21 -0500

    Extract bounce_off_edge! and dx/dy

--- ball.rb
+++ ball.rb
@@ -10,22 +10,26 @@ class Ball
     @speed = 6
   end
 
-  def move!
-    dx = Gosu.offset_x(angle, speed)
-    dy = Gosu.offset_y(angle, speed)
+  def dx; Gosu.offset_x(angle, speed); end
+  def dy; Gosu.offset_y(angle, speed); end
 
+  def move!
     @x += dx
     @y += dy
 
     if @y < 0
       @y = 0
-      @angle = Gosu.angle(0, 0, dx, -dy)
+      bounce_off_edge!
     end
 
     if @y > Pong::HEIGHT
       @y = Pong::HEIGHT
-      @angle = Gosu.angle(0, 0, dx, -dy)
+      bounce_off_edge!
     end
+  end
+
+  def bounce_off_edge!
+    @angle = Gosu.angle(0, 0, dx, -dy)
   end
 
   def x1; @x - SIZE/2; end
