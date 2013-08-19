1c14e6c4587b2ae13309c82acb9ca12f9f499b9a
Michael Fairley <michaelfairley@gmail.com>
2013-08-18 21:10:54 -0500

    Draw scores

--- pong.rb
+++ pong.rb
@@ -13,10 +13,18 @@ class Pong < Hasu::Window
 
   def reset
     @ball = Ball.new
+
+    @left_score = 0
+    @right_score = 0
+
+    @font = Gosu::Font.new(self, "Arial", 30)
   end
 
   def draw
     @ball.draw(self)
+
+    @font.draw(@left_score, 30, 30, 0)
+    @font.draw(@right_score, WIDTH-50, 30, 0)
   end
 
   def update
